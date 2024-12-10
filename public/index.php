60b8f083dac970ee2c6897b0bc759b4f9b9aafcb
AdriaJuarezSTUCOM <pa.2527.ap@gmail.com>
2024-12-10 18:39:21 +0100

    First structure

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Workshop</title>
+</head>
+
+<body>
+    <h1>Select user role</h1>
+    <form action="controllerReparation.php" method="get">
+        <input type="submit" value="client" name="userType">
+        <input type="submit" value="employee" name="userType">
+    </form>
+</body>
+</html>
